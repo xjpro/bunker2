b164655140860595262add0e1c1c28288899fe36
Joshua Prodahl <jprodahl@gmail.com>
2016-12-01 13:04:54 -0600

    session & login

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,9 @@
 var viewController = require('../controllers/viewController');
+var isLoggedIn = require('../policies/isLoggedIn');
 
 module.exports.http = (app) => {
-	app.get('/', viewController.index);
+	app.get('/login', viewController.login);
+	app.get('/', isLoggedIn, viewController.index);
 };
 
 module.exports.socket = (app) => {
--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -1,3 +1,7 @@
+
+module.exports.login = (req, res) => {
+	res.render('login');
+};
 module.exports.index = (req, res) => {
 	res.render('index');
 };
