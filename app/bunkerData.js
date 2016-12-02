5f0c04ae27c7103740a65043b8b92fd8ebaa4b25
Joshua Prodahl <jprodahl@gmail.com>
2016-12-02 15:48:00 -0600

    everyone share a socket

--- app/bunkerData.js
+++ app/bunkerData.js
@@ -1,5 +1,8 @@
 (function (socket) {
-	let subscribers = [];
+	let subscribers = {
+		userUpdated: [],
+		messageReceived: []
+	};
 
 	window.bunkerData = {
 		// Data
@@ -8,18 +11,26 @@
 
 		// Pub/sub
 		subscribe(event, callback) {
-			if (!subscribers[event]) subscribers[event] = [];
 			subscribers[event].push(callback);
 		},
 		unsubscribe(event) {
 			// todo implement unsub
 		},
 
-		// Events
+		// Incoming events
 		init(initialData) {
 			this.user = initialData.user;
-			subscribers['userUpdated'].forEach(callback => callback());
+			subscribers.userUpdated.forEach(callback => callback());
 		},
+		messageReceived(data) {
+			_.find(this.rooms, {_id: data.room}).messages.push(data.text);
+			subscribers.messageReceived.forEach(callback => callback());
+		},
+		messageEdited(data) {
+
+		},
+
+		// Outgoing events
 		createMessage(roomId, messageText) {
 			socket.emit('message', {
 				room: roomId,
@@ -28,4 +39,4 @@
 		}
 	};
 
-})(window.io('localhost'));
+})(window.socket);
--- app/bunkerListener.js
+++ app/bunkerListener.js
@@ -1,5 +1,6 @@
 window.onSignIn = function (googleUser) {
 	localStorage.token = googleUser.getAuthResponse().id_token;
+	window.socket.emit('login', {token: localStorage.token});
 };
 
 (function bunkerListener(socket) {
@@ -7,10 +8,7 @@ window.onSignIn = function (googleUser) {
 	function handleConnected(socket) {
 		console.log('socket connected');
 		console.log(`oauth token is ${localStorage.token ? 'present' : 'not present'}`);
-		if (!localStorage.token) {
-			// show login modal
-		}
-		else {
+		if (localStorage.token) {
 			socket.emit('login', {token: localStorage.token});
 		}
 	}
@@ -18,25 +16,20 @@ window.onSignIn = function (googleUser) {
 	function handleDisconnected(socket) {
 	}
 
-	function handleLoggedIn(socket) {
-		socket.emit('init'); // Yay, now get initial data
-	}
-
-	function handleInitialData(socket, data) {
-		bunkerData.init(data);
-	}
-
 	let events = {
 		connect: handleConnected,
 		disconnect: handleDisconnected,
-		loggedIn: handleLoggedIn,
-		initialData: handleInitialData
+		loggedIn: () => socket.emit('init'),
+		initialData: (socket, data) => bunkerData.init(data),
+		message: (socket, data) => bunkerData.messageReceived(data),
+		edit: (socket, data) => bunkerData.messageEdited(data)
 	};
 
+	// We can listen for events now!
 	_.each(events, (handler, eventName) => {
 		socket.on(eventName, data => {
 			handler(socket, data);
 		});
 	});
 
-})(window.io('localhost'));
+})(window.socket);
